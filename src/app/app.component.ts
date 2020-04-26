4bb6c3accbcb5037439b4aea75859b4ad5581e3e
uday-tavant <uday0108@gmail.com>
2020-04-27 01:21:03 +0530

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,30 @@ const DATA = [
   { rank: 3, name: 'Kobe', surname: 'Bryant', points: 33643 },
   { rank: 4, name: 'Michael', surname: 'Jordan', points: 32292 },
   { rank: 5, name: 'Wilt', surname: 'Chamberlain', points: 31419 },
+  { rank: 6, name: 'Kareem', surname: 'Abdul-Jabbar', points: 38387 },
+  { rank: 7, name: 'Karl', surname: 'Malone', points: 36928 },
+  { rank: 8, name: 'Kobe', surname: 'Bryant', points: 33643 },
+  { rank: 9, name: 'Michael', surname: 'Jordan', points: 32292 },
+  { rank: 10, name: 'Wilt', surname: 'Chamberlain', points: 31419 },
+  { rank: 11, name: 'Kareem', surname: 'Abdul-Jabbar', points: 38387 },
+  { rank: 12, name: 'Karl', surname: 'Malone', points: 36928 },
+  { rank: 13, name: 'Kobe', surname: 'Bryant', points: 33643 },
+  { rank: 14, name: 'Michael', surname: 'Jordan', points: 32292 },
+  { rank: 15, name: 'Wilt', surname: 'Chamberlain', points: 31419 },
+  { rank: 16, name: 'Kareem', surname: 'Abdul-Jabbar', points: 38387 },
+  { rank: 17, name: 'Karl', surname: 'Malone', points: 36928 },
+  { rank: 18, name: 'Kobe', surname: 'Bryant', points: 33643 },
+  { rank: 19, name: 'Michael', surname: 'Jordan', points: 32292 },
+  { rank: 20, name: 'Wilt', surname: 'Chamberlain', points: 31419 },
+  { rank: 21, name: 'Kareem', surname: 'Abdul-Jabbar', points: 38387 },
+  { rank: 22, name: 'Karl', surname: 'Malone', points: 36928 },
+  { rank: 23, name: 'Kobe', surname: 'Bryant', points: 33643 },
+  { rank: 24, name: 'Michael', surname: 'Jordan', points: 32292 },
+  { rank: 25, name: 'Wilt', surname: 'Chamberlain', points: 31419 },
 ];
+
+
+
 
 @Component({
   selector: 'app-demo-datatables-basic',
@@ -16,6 +39,12 @@ const DATA = [
 export class DemoDatatablesBasic {
 
   data = DATA;
+
+  total = this.data.length;
+  pageOptions = ['5','10','15'];
+  selection: string = '5';
+
+  open = false;
 
   // Initial sort
   sort: INglDatatableSort = { key: 'rank', order: 'asc' };
